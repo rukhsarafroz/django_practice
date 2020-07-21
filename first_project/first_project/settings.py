7cc528e3ed1ac89feb95fa2c82a0073440a2389c
rukhsarafroz <rukhsarafroz697@gmail.com>
2020-07-21 22:04:04 +0530

    change setting to run server at localhost:8000

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=!mh=5+a&7#eugl_st+6o^%&$24@r6c1s368_^s20gzjq$5dvx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
