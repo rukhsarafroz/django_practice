523904d2fa2cd6bab7e9afa477851a8f71d207f2
rukhsarafroz <rukhsarafroz697@gmail.com>
2020-07-21 22:39:10 +0530

    add path into first app

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from first_app import views
 
 urlpatterns = [
+    path('first_app/',include('first_app.urls')),
+    path('hello/',views.index,name='index'),
     path('admin/', admin.site.urls),
 ]
