799512611543b2b49ad852741596a678f511ec80
rukhsarafroz <rukhsarafroz697@gmail.com>
2020-07-23 22:14:48 +0530

    create template for first app

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello World")
+    my_dictionary = {'insert_me': "Now I am coming from"}
+    return render(request, 'first_app/index.html', context=my_dictionary)
