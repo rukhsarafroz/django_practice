a6a13dce1789cde1790ebcfc7978d18ef8ac0aee
rukhsarafroz <rukhsarafroz697@gmail.com>
2020-07-24 17:13:57 +0530

    show access record into table using model view template

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import AccessRecord, Topic, Webpage
 # Create your views here.
 
 
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_record': webpages_list}
     my_dictionary = {'insert_me': "Now I am coming from"}
-    return render(request, 'first_app/index.html', context=my_dictionary)
+    return render(request, 'first_app/index.html', context=date_dict)
